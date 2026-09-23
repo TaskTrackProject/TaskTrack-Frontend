3a4b28cfbc4239a2fe070ac930595b592b97bfdf
ThanhHuongK19 <daohuong030303@gmail.com>
2026-09-23 08:21:32 +0700

    fix: use production backend api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tasktrack-backend.onrender.com/api'
+  apiUrl: 'https://tasktrack-backend-7lxe.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://tasktrack-backend.onrender.com/api'
+  apiUrl: 'https://tasktrack-backend-7lxe.onrender.com/api'
 };
 
 // export const environment = {
